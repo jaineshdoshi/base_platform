4e0c1de86670b54d1ef54b6fe1395964dc6bf689
Jainesh <jaineshpdoshi@gmail.com>
2016-08-18 04:56:52 +0530

    Resolved pointer issues, Modified MMU

--- mmu/mmu.cpp
+++ mmu/mmu.cpp
@@ -39,7 +39,7 @@
 //////////////////////////////////////////////////////////////////////////////
 
 // Uncomment this for the debug model
-//#define MMU_DEBUG
+#define MMU_DEBUG
 
 
 /// Constructor
@@ -65,8 +65,16 @@ ac_tlm_rsp ac_tlm_mmu::transport(const ac_tlm_req &request)
   ac_tlm_rsp response;
 
 // Check the address requested
-  if(request.addr >= 0x80000000){
-    // Virtual memory is being accessed in this request
+  if(request.addr >= 0x80000000 && request.addr < 0xA0000000){
+    //! kseg0 0x8000 0000-9FFF FFFF (512MB):
+    //! These addresses are translated
+    //! into physical addresses by merely stripping off the top bit and mapping
+    //! them contiguously into the low 512MB of physical memory.
+    //! Addresses in this region are almost always accessed through the cache,
+    //! so they may not be used until the caches are properly initialized. They
+    //! will be used for most programs and data in systems not using the MMU
+    //! and will be used for the OS kernel for systems that do use the MMU.
+
     ac_tlm_req req = request;
     req.addr = request.addr - 0x80000000;
 #ifndef MMU_DEBUG
@@ -74,7 +82,24 @@ ac_tlm_rsp ac_tlm_mmu::transport(const ac_tlm_req &request)
 #endif
     response = BUS_port->transport(req);
     return response;
-  } else {
+  }
+
+  else if(request.addr >= 0xA0000000 && request.addr < 0xC0000000){
+    //! ksegl 0xA000 0000-BFFF FFFF (512MB):
+    //! These addresses are mapped into physical addresses by stripping off the
+    //! leading 3 bits, giving a duplicate mapping of the low 512MB of physical
+    //! memory. But this time, access will not use the cache. Use this region to
+    //! access your initial program ROM, and for I/O registers.
+
+    ac_tlm_req req = request;
+    req.addr = request.addr & 0x1FFFFFFF;
+#ifndef MMU_DEBUG
+    cout << "Actual Address to Bus (Memory): "<< req.addr << endl;
+#endif
+    response = BUS_port->transport(req);
+    return response;
+  }
+  else {
     // Actual memory accessed
 #ifndef MMU_DEBUG
 //    cout <<"Address requested"<< request.addr << endl;
